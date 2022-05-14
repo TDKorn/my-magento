c2d267e5e7035d636c9273123849ce0c802fd9d4
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-05-14 00:13:52 -0400

    Update utils
    * Remove utils folder/package and replace with utils.py
    * Remove UserAgent class and replace with get_agent() function
    * get_agent() -> function that scrapes most common user agent

---
+++ magento/utils.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as bs
+
+BACKUP_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'
+
+
+def get_agent():
+    r = requests.get('https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome')
+    if r.ok:
+        soup = bs(r.text, 'html.parser')
+        if soup_agents := soup.find_all('span', {'class': 'code'}):
+            return soup_agents[0].text
+    # If function fails to scrape, will use hardcoded user agent
+    return BACKUP_AGENT
--- magento/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .UserAgent import UserAgent
