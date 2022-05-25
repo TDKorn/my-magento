78cd3f77a7abeb97a7fa5706ba653751635f8699
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-05-25 02:33:22 -0400

    Update setup.py
    HOW did I not see that??? 😭

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     packages=['magento'],
     version='1.1',
     license='MIT',
-    description='Python Magento 2 REST API Client'
+    description='Python Magento 2 REST API Client',
     author='Adam Korn',
     author_email='hello@dailykitten.net',
     url='https://www.github.com/TDKorn/my-magento',
     download_url="https://github.com/TDKorn/my-magento/tarball/master",
-    keywords=["magento", "magento-api", "python-magento", "python", "python3", "magento-python", "pymagento", "py-magento", "magento2", "magento-2", "magento2-api"]
-    install_requires=["requests", "beautifulsoup4"],
+    keywords=["magento", "magento-api", "python-magento", "python", "python3", "magento-python", "pymagento", "py-magento", "magento2", "magento-2", "magento2-api"],
+    install_requires=["requests", "beautifulsoup4"]
 )
