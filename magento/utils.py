6ae639d08026e18165c61733d1973b4449dd449d
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-05-25 08:45:21 -0400

    Update utils.get_agent() to reduce dependencies
    utils.py
    * Do the parsing myself so that beautifulsoup isn't needed to install
    
    I think everyone has requests so I will not attempt to change that

--- magento/utils.py
+++ magento/utils.py
@@ -1,17 +1,4 @@
 import requests
-from bs4 import BeautifulSoup as bs
-
-BACKUP_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
-
-
-def get_agent():
-    r = requests.get('https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome')
-    if r.ok:
-        soup = bs(r.text, 'html.parser')
-        if soup_agents := soup.find_all('span', {'class': 'code'}):
-            return soup_agents[0].text
-    # If function fails to scrape, will use hardcoded user agent
-    return BACKUP_AGENT
 
 
 class ItemManager:
@@ -28,3 +15,26 @@ class ItemManager:
 
     def sum_attrs(self, attr):
         return sum(self.get_attrs(attr))
+
+
+AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+]
+
+
+def get_agents() -> []:
+    """Scrapes a list of user agents. Returns a default list if the scrape fails."""
+    if (response := requests.get('https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome')).ok:
+        section = response.text.split('<h2>Latest Chrome on Windows 10 User Agents</h2>')[1]
+        raw_agents = section.split('code\">')[1:]
+        agents = [agent.split('<')[0] for agent in raw_agents]
+        for a in agents:
+            if a not in AGENTS:
+                AGENTS.append(a)
+    # If function fails will return the hardcoded list
+    return AGENTS
+
+
+def get_agent() -> str:
+    """Returns a single user agent string"""
+    return get_agents()[0]
--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     url='https://www.github.com/TDKorn/my-magento',
     download_url="https://github.com/TDKorn/my-magento/tarball/master",
     keywords=["magento", "magento-api", "python-magento", "python", "python3", "magento-python", "pymagento", "py-magento", "magento2", "magento-2", "magento2-api"],
-    install_requires=["requests", "beautifulsoup4"]
+    install_requires=["requests"]
 )
