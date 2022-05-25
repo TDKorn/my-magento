e7fae20378cc3cb18dbdaff5d8bce4e4ecbe8781
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-05-25 02:27:20 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='MyMagento',
+    packages=['magento'],
+    version='1.1',
+    license='MIT',
+    description='Python Magento 2 REST API Client'
+    author='Adam Korn',
+    author_email='hello@dailykitten.net',
+    url='https://www.github.com/TDKorn/my-magento',
+    download_url="https://github.com/TDKorn/my-magento/tarball/master",
+    keywords=["magento", "magento-api", "python-magento", "python", "python3", "magento-python", "pymagento", "py-magento", "magento2", "magento-2", "magento2-api"]
+    install_requires=["requests", "beautifulsoup4"],
+)
