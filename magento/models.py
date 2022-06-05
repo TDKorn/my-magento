0ae7335af24e70954b92bfa85032c63433142288
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-06-04 23:34:49 -0400

    Update models.py - Add docstrings, private_keys param
    models.py
    * Trying to add proper docstrings so I can eventually create documentation. This package really needs documentation
    * Add private_keys param to Model initialization
        - If set to true (default), will take all keys in the "excluded_keys" list and set them as attributes prefixed with "_"
        - This way the data is still there, but it doesn't cause issues with properties
    
    * Update Product media functions to properly use new MediaEntry class
    
    NOTE: MediaEntry is a Model, but it doesn't have a searchable endpoint
    
    Unsure if I should
        - Create a SearchQuery class that doesn't have any use aside from maintaining consistency
        - Change parse to use a backup way to create objects
        - Leave it as is, because no methods within the MediaEntry class will result in the need to create more MediaEntry objects

--- magento/models.py
+++ magento/models.py
@@ -1,15 +1,36 @@
 # Everything without entity_id
 from __future__ import annotations
-import copy
 import requests
-from . import clients
+
 from abc import ABC, abstractmethod
+from typing import Union
+from . import clients
 
 
 class Model(ABC):
+    """The base class for all API response wrapper classes
 
-    def __init__(self, data: dict, client: clients.Client, endpoint: str):
-        """The base class for all API response wrapper classes"""
+    Each Model subclass is a representation of an API response, typically from an endpoint that can
+    be searched/filtered using search criteria. Thus, most Models also have a corresponding SearchQuery subclass,
+    accessible through its query_endpoint() method
+    """
+
+    def __init__(self, data: dict, client: clients.Client, endpoint: str, private_keys: bool = True):
+        """Initialize a Model object using the json dict from an API response
+
+        :param data: The API response to use as the source data for the object
+
+        :param client: An initialized Client object. Note that the Client does not need to be logged in, or have a
+            valid access token; the credentials will be authenticated as needed
+
+        :param endpoint: The base endpoint that will be used for any API requests; used to match the object with its
+            corresponding SearchQuery object, if it exists
+
+        :param private_keys: If set to True, the keys denoted in the excluded_keys property will be set as attributes
+            prefixed with "_". For example, if "status" is an excluded key,  and private_keys=True, the object
+            will be initialized with an "_status" attribute instead of completely excluding it.
+        """
+
         if not isinstance(data, dict):
             raise TypeError(f'Parameter "data" must be of type {dict}')
         if not isinstance(endpoint, str):
@@ -19,16 +40,15 @@ class Model(ABC):
 
         self.client = client
         self.endpoint = endpoint
-        self.set_attrs(data)
+        self.set_attrs(data, private_keys=private_keys)
 
-    @property
-    @abstractmethod
-    def excluded_keys(self):
-        """Keys that should not be set by set_attrs() method"""
-        pass
+    def set_attrs(self, data: dict, private_keys: bool = True) -> None:
+        """Sets object attributes, using an API response dict as the data source
 
-    def set_attrs(self, data: dict, set_private: bool = True):
-        """Initializes the object attributes using a dictionary as a data source"""
+        :param data: The API response to use as the object source data
+        :param private_keys: If set to True, will set the keys in the "excluded_keys" property as attributes
+                                prefixed with an "_", instead of fully excluding them
+        """
         keys = set(data) - set(self.excluded_keys)
         for key in keys:
             if key == 'custom_attributes':
@@ -37,22 +57,38 @@ class Model(ABC):
             else:
                 setattr(self, key, data[key])
 
-        if set_private:
+        if private_keys:
             for key in self.excluded_keys:
                 setattr(self, "_" + key, data.get(key))
 
+    @property
+    @abstractmethod
+    def excluded_keys(self) -> list[str]:
+        """Keys that should not be set by set_attrs() method
+
+        :returns the list of API response keys that should not be set as attributes
+        """
+        pass
+
+    @staticmethod
+    def unpack_attributes(attributes: list):
+        """Unpacks a list of custom attribute dictionaries into a single dictionary
+
+        :param attributes: A list of custom attribute dictionaries of the form
+                            [{'attribute_code': 'attr', 'value': 'val'},]
+        """
+        return {attr['attribute_code']: attr['value'] for attr in attributes}
 
     def query_endpoint(self):
         """Depending on the endpoint, will return either a SearchQuery or a SearchQuery subclass"""
         return self.client.search(self.endpoint)
 
-    def parse(self, response):
-        return self.query_endpoint().parse(response)
+    def parse(self, response: dict):
+        """If a SearchQuery subclass exists, initializes a Model of the same type from an API response
 
-    @staticmethod
-    def unpack_attributes(attributes):
-        """Unpacks a list of custom attribute dictionaries into a single dictionary"""
-        return {attr['attribute_code']: attr['value'] for attr in attributes}
+        :param response: The API response dict to generate the object from
+        """
+        return self.query_endpoint().parse(response)
 
 
 class Product(Model):
@@ -70,9 +106,9 @@ class Product(Model):
         super().__init__(
             data=data,
             client=client,
-            endpoint='products'
+            endpoint='products',
+            private_keys=True
         )
-        self._media_gallery_entries = []
 
     def __str__(self):
         return f'Magento Product: {self.sku}'
@@ -82,15 +118,35 @@ class Product(Model):
         return ['media_gallery_entries']
 
     @property
-    def
+    def media_gallery_entries(self):
+        return [MediaEntry(self, entry) for entry in self._media_gallery_entries]
 
     @property
     def thumbnail(self):
-        return self._get_media_entry('thumbnail', 'in', 'types')
+        return self._get_media_entry('thumbail', 'in', 'types')
+
+    def get_media(self, entry_id):
+        return self._get_media_entry('id', '==', entry_id)
+
+    def _get_media_entry(self, val: Union[str, int], condition: str, attribute: str):
+        if not isinstance(val, int):
+            val = f'\"{val}\"'
+
+        for entry in self.media_gallery_entries:
+            if hasattr(entry, attribute):
+                attr = getattr(entry, attribute)
+                criteria = f'{val} {condition} {attr}'
+
+                try:
+                    if eval(criteria):
+                        return entry
+
+                except Exception as e:
+                    raise RuntimeError(f'Invalid Criteria: {criteria}') from e
 
     @property
     def thumbnail_link(self):
-        return self.get_media_link(self.thumbnail)
+        return self.thumbnail.link
 
     @property
     def encoded_sku(self):
@@ -188,8 +244,8 @@ class Product(Model):
                 sep='\n'
             )
 
-class MediaEntry(Model):
 
+class MediaEntry(Model):
     MEDIA_MAPPING = {
         'id': int,
         'media_type': str,
@@ -208,27 +264,17 @@ class MediaEntry(Model):
         )
         self.product = product
 
-    def get_media(self, entry_id):
-        return self._get_media_entry('id', '==', entry_id)
+    @property
+    def excluded_keys(self):
+        return []
 
-    def get_media_link(self, entry):
-        return f'https://{self.client.domain}/media/catalog/product{entry["file"]}'
+    @property
+    def is_thumbnail(self):
+        return 'thumbnail' in self.types
 
-    def _get_media_entry(self, val, condition, key):
-        for entry in self.media_gallery_entries:
-            if not isinstance(val, int):
-                val = f'"{val}"'
-
-            criteria = f'{val} {condition} entry["{key}"]'
-            try:
-                if eval(criteria):
-                    return copy.deepcopy(entry)
-
-            except Exception as e:
-                raise RuntimeError(f'Invalid Criteria: {criteria}') from e
-
-
-
+    @property
+    def link(self):
+        return f'https://{self.client.domain}/media/catalog/product{self.file}'
 
 
 class Category(Model):
