997d0cb9043f55bca54293cb14c3bf18fcd0470f
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-06-05 11:37:16 -0400

    Update logger_test.py
    not sure how to write tests still but this looks better i guess

--- tests/logger_test.py
+++ tests/logger_test.py
@@ -14,6 +14,7 @@ logger = MagentoLogger(
     log_file='logger_test.log',
     stdout_level='DEBUG'
 )
+
 logger.debug('Beginning logger test')
 logger.debug('Logging in to client...')
 try:
@@ -22,43 +23,44 @@ try:
         username=USERNAME,
         password=PASSWORD
     )
-    api.logger.debug('Logged in from {}'.format(
-        os.path.abspath('logger_test.py'))
-    )
-    logger.debug('Client logged in.')
 
 except AuthenticationError as e:
-    logger.error(e)
-    logger.debug('Failed to authenticate credentials. Test requires login.')
+    logger.info('Failed to obtain an access token. Please ensure your credentials are corred')
     sys.exit(-1)
+
+logger.debug('Client logged in.')
+api.logger.debug('Using Client logger to log to {}'.format(__file__))  # Is that a weird test...?
 
 logger.debug('Verifying correct logger is returned from logger attribute and get_logger() method...')
 attr = api.logger.logger
 meth = api.get_logger().logger
 assert attr == meth
 
-logger.debug('Checking handlers...')
-handlers = [h for h in attr.handlers]
-assert len(handlers) < 4
-
-logger.debug('Checking file handlers...')
-f_handlers = [f for f in handlers if isinstance(f, logging.FileHandler)]
-assert len(f_handlers) == 2
-f1, f2 = f_handlers
-
-
-def fname_from_handler(f_handler):
-    return os.path.basename(f_handler.baseFilename)
-
-
-logger.debug('Checking log files...')
+logger.debug('Verifying magento and Client log FileHandlers are present...')
 magento_log = MagentoLogger.PACKAGE_LOG_NAME + '.log'
 client_log = MagentoLogger.CLIENT_LOG_NAME.format(
     DOMAIN=DOMAIN.split('.')[0],
-    USERNAME=USERNAME
-) + '.log'
+    USERNAME=USERNAME) + '.log'
 
-assert magento_log in map(fname_from_handler, [f1, f2])
-assert client_log in map(fname_from_handler, [f1, f2])
+
+def get_filehandlers(logger: logging.Logger):
+    """Get all the FileHandlers handling a logger"""
+    return [handler for handler in logger.handlers if isinstance(handler, logging.FileHandler)]
+
+
+def get_logfiles(logger):
+    """Get the log file paths from all FileHandlers of a logger"""
+    return [handler.baseFilename for handler in get_filehandlers(logger)]
+
+
+assert os.path.abspath(magento_log) in get_logfiles(api.logger.logger)
+assert os.path.abspath(client_log) in get_logfiles(api.logger.logger)
+logger.debug('The Client is writing to both the Client and package log files ')
+
+requests_logger = logging.getLogger('urllib3.connectionpool')
+assert os.path.abspath(magento_log) in get_logfiles(requests_logger)
+assert os.path.abspath(client_log) in get_logfiles(requests_logger)
+logger.debug('The requests package logger is writing to both the Client and package log files')
+
 logger.debug('All tests passed')
 sys.exit(0)
