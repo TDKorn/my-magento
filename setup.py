3d13e022ee083c2dc16c419090b655f6726368df
TDKorn <96394652+TDKorn@users.noreply.github.com>
2022-06-13 22:30:32 -0400

    Bump version for PyPi 🤩
    * I can't believe I actually made it to this point
    * Also I can't believe that I could've done this almost a month ago? Like everything was set up already why did I think there was so much more I had to do
    
    Anyways squashed:
    commit cf2660cd2d04a8613ce285c3e1721fa77638dd8b
    Author: TDKorn <96394652+TDKorn@users.noreply.github.com>
    Date:   Mon Jun 13 22:25:14 2022 -0400
    
        Update setup.py
    
        * It was actually me that was using the name I'm dumb
    
    commit c9bebf179724c743a51c249877deeac29938649c
    Author: TDKorn <96394652+TDKorn@users.noreply.github.com>
    Date:   Mon Jun 13 12:55:14 2022 -0400
    
        Damn i cant use that
    
    commit 6e8c6f9487a2ab849a044316251b47e2671dd958
    Author: TDKorn <96394652+TDKorn@users.noreply.github.com>
    Date:   Mon Jun 13 12:49:52 2022 -0400
    
        Bum p for PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='MyMagento',
+    name='my-magento',
     packages=['magento'],
-    version='1.1.0',
+    version='1.1.1',
     license='MIT',
     description='Python Magento 2 REST API Client',
     author='Adam Korn',
