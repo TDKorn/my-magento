fb502cf7ea7a0572e9e59ffefb261c97e5db6b78
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-01-04 06:02:48 -0500

    Update exceptions.py
    * Fix message parsing for int values
    * And how could i forget about enumerate 😩

--- magento/exceptions.py
+++ magento/exceptions.py
@@ -54,18 +54,18 @@ class MagentoError(Exception):
 
                 if err_params:
                     for param in err_params:
-                        err_msg = err_msg.replace(f'%{param}', err_params[param])
+                        err_msg = err_msg.replace(f'%{param}', f'{err_params[param]}')
 
                 message += '\n' + err_msg
 
         if params:
             if isinstance(params, dict):
                 for param in params:  # Same format as error params
-                    message = message.replace(f'%{param}', params[param])
+                    message = message.replace(f'%{param}', f'{params[param]}')
 
-            else:  # List of param values
-                for i in range(len(params)):  # Message parameters use %{index}, starting from 1
-                    message = message.replace(f'%{i+1}', params[i])
+            elif isinstance(params, list):  # List of param values
+                for i, param in enumerate(params, 1):  # Message has params as %{index}, starting from 1
+                    message = message.replace(f'%{i}', param)
 
         return message
 
