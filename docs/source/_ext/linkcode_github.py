41b001cb6ea14ae740ea436e887acd3ca7c4ef43
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-02-01 19:55:38 -0500

    Update linkcode_github.py
    Simplify tf out of it

--- docs/source/_ext/linkcode_github.py
+++ docs/source/_ext/linkcode_github.py
@@ -1,53 +1,24 @@
-"""Replace the nodes from linkcode with nodes that use a "linkcode-link" class (for CSS styling) and customizable text"""
-
+import sphinx
+from sphinx.locale import _
+from sphinx.application import Sphinx
+from docutils.nodes import Node, Text
+from docutils import nodes
 from typing import Any, Dict
 
-from docutils import nodes
-from docutils.nodes import Node
 
-import sphinx
-from sphinx import addnodes
-from sphinx.application import Sphinx
-from sphinx.locale import _
-
-
-def doctree_read(app: Sphinx, doctree: Node) -> None:
+def add_linkcode_node_class(app: Sphinx, doctree: Node, docname: str) -> None:
+    """Modifies the nodes added by linkcode to use the "linkcode-link" class and custom link text"""
     env = app.builder.env
-
     link_text = getattr(env.config, 'linkcode_link_text')
-    linkcode_url = getattr(env.config, 'linkcode_url')
 
-    for objnode in list(doctree.findall(addnodes.desc)):
-        if objnode.get('domain') != 'py':
-            continue
-
-        for signode in objnode:
-            if not isinstance(signode, addnodes.desc_signature):
-                continue
-
-            for child in signode.children:
-                if not isinstance(child, addnodes.only):
-                    continue
-
-                onlynode = child
-                for only_child in onlynode.children:
-                    if not isinstance(only_child, nodes.reference):
-                        continue
-
-                    ref_node = only_child
-                    refuri = ref_node.get("refuri", None)
-                    if not refuri.startswith(linkcode_url.split('{')[0]):
-                        continue
-
-                    new_inline = nodes.inline('', _(f'{link_text}'), classes=['linkcode-link'])
-                    new_onlynode = addnodes.only(expr='html')
-                    new_onlynode += nodes.reference('', '', new_inline, internal=False, refuri=refuri)
-
-                    onlynode.replace_self(new_onlynode)
+    for node in list(doctree.findall(nodes.inline)):
+        if 'viewcode-link' in node['classes']:
+            if node.parent.get('internal', None) is False:
+                node['classes'] = ['linkcode-link']
+                node.children = [Text(_(f'{link_text}'))]
 
 
 def setup(app: Sphinx) -> Dict[str, Any]:
-    app.connect('doctree-read', doctree_read)
+    app.connect('doctree-resolved', add_linkcode_node_class)
     app.add_config_value('linkcode_link_text', '[source]', '')
-    app.add_config_value('linkcode_url', None, '')
     return {'version': sphinx.__display_version__, 'parallel_read_safe': True}
