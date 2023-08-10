19dd13fd2a72bd5e8c2798113e5d6d7eafa918ff
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-08-10 15:34:32 -0400

    Reorganize utils

--- magento/utils.py
+++ magento/utils.py
@@ -8,6 +8,9 @@ from typing import Union, List, Type
 from logging import Logger, FileHandler, StreamHandler, Handler
 
 
+AGENTS = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36']
+
+
 def parse_domain(domain: str):
     """Returns the root domain of the provided domain
 
@@ -25,20 +28,22 @@ def parse_domain(domain: str):
     raise ValueError("Invalid format provided for ``domain``")
 
 
-class ItemManager:
+def get_agents() -> list:
+    """Scrapes a list of user agents. Returns a default list if the scrape fails."""
+    if (response := requests.get('https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome')).ok:
+        section = response.text.split('<h2>Latest Chrome on Windows 10 User Agents</h2>')[1]
+        raw_agents = section.split('code\">')[1:]
+        agents = [agent.split('<')[0] for agent in raw_agents]
+        for a in agents:
+            if a not in AGENTS:
+                AGENTS.append(a)
+    # If function fails, will still return the hardcoded list
+    return AGENTS
 
-    def __init__(self, items=None):
-        self.items = items if items else []
 
-    def add(self, item):
-        if item not in self.items:
-            self.items.append(item)
-
-    def get_attrs(self, attr):
-        return [getattr(item, attr, 0) for item in self.items]
-
-    def sum_attrs(self, attr):
-        return sum(self.get_attrs(attr))
+def get_agent(index=0) -> str:
+    """Returns a single user agent string from the specified index of the AGENTS list"""
+    return get_agents()[index]  # Specify index only if you hardcode more than 1
 
 
 class LoggerUtils:
@@ -352,27 +357,6 @@ class MagentoLogger:
                 req_logger.addHandler(handler)
 
         return True
-
-
-AGENTS = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36']
-
-
-def get_agents() -> list:
-    """Scrapes a list of user agents. Returns a default list if the scrape fails."""
-    if (response := requests.get('https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome')).ok:
-        section = response.text.split('<h2>Latest Chrome on Windows 10 User Agents</h2>')[1]
-        raw_agents = section.split('code\">')[1:]
-        agents = [agent.split('<')[0] for agent in raw_agents]
-        for a in agents:
-            if a not in AGENTS:
-                AGENTS.append(a)
-    # If function fails, will still return the hardcoded list
-    return AGENTS
-
-
-def get_agent(index=0) -> str:
-    """Returns a single user agent string from the specified index of the AGENTS list"""
-    return get_agents()[index]  # Specify index only if you hardcode more than 1
 
 
 def get_package_file_handler():
